83cd27c9c587a07de5d52261312acadf8d3147ed
Milan Shah <mshah1992@live.com>
2020-06-10 20:25:46 -0400

    Added mutations and resolvers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from './schema';
+import { schema } from './schema';
 
 const app = express();
 
@@ -8,20 +8,8 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
 
-const root = { friend: () => {
-    return {
-        "id": 1234567890,
-        "firstName": "Milan",
-        "lastName": "Shah",
-        "gender": "Male",
-        "language": "English",
-        "email": "test@test.com"
-    }
-} };
-
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
 }));
 
